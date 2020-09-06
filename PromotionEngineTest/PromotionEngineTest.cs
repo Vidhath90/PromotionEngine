4080811408194676ac64bff06046a4adafda0fc2
vidhath <vidhath.herle@honeywell.com>
2020-09-06 13:56:34 +0530

    updated

--- PromotionEngineTest/PromotionEngineTest.cs
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -42,5 +42,27 @@ namespace PromotionEngineTest
             Assert.AreEqual(actualTotal, total, 0, "Wrong Total");
 
         }
+
+        [TestMethod]
+        public void PromotionEngineTest_CalculateTotal()
+        {
+
+            //Arrange
+            int actualTotal = 205;
+            Dictionary<string, int> userCart = new Dictionary<string, int>();
+            userCart.Add("A", 3);
+            userCart.Add("B", 2);
+            userCart.Add("C", 1);
+            userCart.Add("D", 1);
+            PromotionEngine pe = new PromotionEngine();
+
+            //Act
+            int total = pe.CalculateTotalAfterPromotions(userCart);
+
+            //Assert
+            Assert.AreEqual(actualTotal, total, 0, "Correct Total");
+
+        }
+
     }
 }
