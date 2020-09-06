e24b02a912cc0714891ad4420a4d621131657660
vidhath <vidhath.herle@honeywell.com>
2020-09-06 12:09:21 +0530

    Restructuring

---
+++ PromotionEngineLibrary/ActivePromotions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngineLibrary
+{
+    public static class ActivePromotions
+    {
+        public static Dictionary<string,PerItemQuantityPrice> PromotionPrice
+        { get; set; }
+
+    }
+    public class PerItemQuantityPrice
+    {
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ PromotionEngineTest/PromotionEngineTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PromotionEngineLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineTest
+{
+    
+        [TestClass]
+        public class PromotionEngineTest
+        {
+            [TestMethod]
+            public void busFareCalculationTest1()
+            {
+                //Arrange
+                int startPt = 2;
+                int endPt = 6;
+                int fare = 10;
+                int stageValue = 5;
+                CalculateFare cf = new CalculateFare();
+
+                //Act
+                int fareTestValue = cf.CalculateBusFareBasedOnStops(startPt, endPt, stageValue);
+
+                //Assert
+                Assert.AreEqual(fare, fareTestValue, 0, "Proper Fare");
+            }
+        }
+}
