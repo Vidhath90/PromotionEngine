de7aa7abb1de7949d7c035eeaa5c09c14335c1a4
vidhath <vidhath.herle@honeywell.com>
2020-09-06 11:46:30 +0530

    initial checkin- with classes

---
+++ PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+   public  class PromotionEngine
+    {
+        private Dictionary<string, PerItemQuantityPrice> activePromotions = new Dictionary<string, PerItemQuantityPrice>();
+        public PromotionEngine()
+        {
+            FillActivePromotions();
+        }
+        private void FillActivePromotions()
+        {
+            activePromotions.Add("A", new PerItemQuantityPrice { Quantity = 3, Price = 130 });
+            activePromotions.Add("B", new PerItemQuantityPrice { Quantity = 2, Price = 45 });
+            activePromotions.Add("C", new PerItemQuantityPrice { Quantity = 0, Price = 0 });
+            activePromotions.Add("D", new PerItemQuantityPrice { Quantity = 0, Price = 0 });
+            activePromotions.Add("C&D", new PerItemQuantityPrice { Quantity = 1, Price = 30 });
+            ActivePromotions.PromotionPrice = activePromotions;
+        }
+        public int CalculateTotalAfterPromotions(Dictionary<string,int> userCart)
+        {
+
+            int total = 0; 
+
+            return total;
+        }
+
+    }
+}
---
+++ PromotionEngine/UnitPricing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public static class UnitPricing
+    {
+        public static Dictionary<string, int> UnitPrice = new Dictionary<string, int>()
+        {
+            {"A",50 },
+            {"B",30 },
+            {"C",20 },
+            {"D",15 }
+        };
+        
+    }
+}
